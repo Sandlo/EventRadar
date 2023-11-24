c252d8ac18912471a0bbd89643a293c61207f743
domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2023-11-24 13:15:17 +0100

    Create empty activities

---
+++ app/src/main/java/com/example/eventradar/activities/TicketActivity.kt
@@ -0,0 +1,12 @@
+package com.example.eventradar.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.eventradar.R
+
+class TicketActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ticket)
+    }
+}
--- app/src/main/java/com/example/eventradar/fragments/TicketsFragment.kt
+++ app/src/main/java/com/example/eventradar/fragments/TicketsFragment.kt
@@ -1,11 +1,15 @@
 package com.example.eventradar.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.eventradar.R
+import com.example.eventradar.activities.EventActivity
+import com.example.eventradar.activities.TicketActivity
 
 class TicketsFragment : Fragment() {
 
@@ -15,6 +19,10 @@ class TicketsFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View {
         val root = inflater.inflate(R.layout.fragment_tickets, null)
+
+        root.findViewById<Button>(R.id.dummy_button).setOnClickListener {
+            requireContext().startActivity(Intent(requireContext(), TicketActivity::class.java))
+        }
 
         return root
     }
