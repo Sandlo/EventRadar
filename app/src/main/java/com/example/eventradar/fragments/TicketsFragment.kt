f061e8ac63dae79abdf5a1839576062124333086
domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2023-11-25 18:29:08 +0100

    Implement default list

--- app/src/main/java/com/example/eventradar/fragments/TicketsFragment.kt
+++ app/src/main/java/com/example/eventradar/fragments/TicketsFragment.kt
@@ -5,12 +5,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.eventradar.R
 import com.example.eventradar.activities.TicketActivity
+import com.example.eventradar.adapters.SimpleListAdapter
+import com.example.eventradar.data.SimpleListItem
+import com.example.eventradar.interfaces.RecyclerViewHelperInterface
 
-class TicketsFragment : Fragment() {
+class TicketsFragment : Fragment(), RecyclerViewHelperInterface {
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -19,10 +24,24 @@ class TicketsFragment : Fragment() {
     ): View {
         val root = inflater.inflate(R.layout.fragment_tickets, container, false)
 
-        root.findViewById<Button>(R.id.dummy_button).setOnClickListener {
-            requireContext().startActivity(Intent(requireContext(), TicketActivity::class.java))
-        }
+        val dummyItem = SimpleListItem(
+            "Event",
+            "1. April 2023 • 5,00 €",
+            R.drawable.ic_circle_tag
+        )
+
+        val recyclerView = root.findViewById<RecyclerView>(R.id.list)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = SimpleListAdapter(
+            Array(20) { dummyItem }.asList(),
+            this
+        )
 
         return root
+    }
+
+    override fun onItemClicked(view: View, position: Int) {
+        Toast.makeText(requireContext(), position.toString(), Toast.LENGTH_SHORT).show()
+        requireContext().startActivity(Intent(requireContext(), TicketActivity::class.java))
     }
 }
---
+++ app/src/main/java/com/example/eventradar/interfaces/RecyclerViewHelperInterface.kt
@@ -0,0 +1,7 @@
+package com.example.eventradar.interfaces
+
+import android.view.View
+
+interface RecyclerViewHelperInterface {
+    fun onItemClicked(view: View, position: Int)
+}
