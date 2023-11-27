e144c158faf5506f9d3fcea2da75b3ac5ddd1f60
domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2023-11-27 17:37:14 +0100

    Fix status bar color

--- app/src/main/java/com/example/eventradar/activities/BaseActivity.kt
+++ app/src/main/java/com/example/eventradar/activities/BaseActivity.kt
@@ -1,14 +1,19 @@
 package com.example.eventradar.activities
 
 import android.annotation.SuppressLint
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.eventradar.R
 import com.google.android.material.elevation.SurfaceColors
 
 @SuppressLint("Registered")
 open class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (resources.configuration.uiMode.and(
+                Configuration.UI_MODE_NIGHT_MASK
+        ) != Configuration.UI_MODE_NIGHT_YES) setTheme(R.style.LightStatusBarOverlay)
         val color = SurfaceColors.SURFACE_2.getColor(this)
         window.statusBarColor = color
         window.navigationBarColor = color
