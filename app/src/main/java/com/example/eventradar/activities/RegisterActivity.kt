43c50b02d9bdaf8f3f77ea7f535bdb8a73169150
TimIsKing <zuehlke.tim2@gmail.com>
2023-11-29 15:23:57 +0100

    Textfelder und Button

--- app/src/main/java/com/example/eventradar/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/eventradar/activities/RegisterActivity.kt
@@ -2,15 +2,15 @@ package com.example.eventradar.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import com.example.eventradar.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class RegisterActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        findViewById<Button>(R.id.dummy_button).setOnClickListener {
+        findViewById<FloatingActionButton>(R.id.floating_action_button).setOnClickListener {
             startActivity(Intent(this, InterestsActivity::class.java))
         }
     }
