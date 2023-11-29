02717b1a10f4412966fc9f5833f26fb2f9ab57cc
TimIsKing <zuehlke.tim2@gmail.com>
2023-11-29 15:57:31 +0100

    Booking Liste und Buttom

--- app/src/main/java/com/example/eventradar/activities/BookingActivity.kt
+++ app/src/main/java/com/example/eventradar/activities/BookingActivity.kt
@@ -2,16 +2,38 @@ package com.example.eventradar.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.eventradar.R
+import com.example.eventradar.adapters.SimpleListAdapter
+import com.example.eventradar.data.SimpleListItem
+import com.example.eventradar.interfaces.RecyclerViewHelperInterface
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class BookingActivity : BaseActivity() {
+class BookingActivity : BaseActivity(), RecyclerViewHelperInterface {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_booking)
 
-        findViewById<Button>(R.id.dummy_button).setOnClickListener {
+        val recyclerView = findViewById<RecyclerView>(R.id.list)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = SimpleListAdapter(
+            listOf(
+                SimpleListItem("Placeholder", resources.getString(R.string.booking_title)),
+                SimpleListItem("Placeholder", resources.getString(R.string.booking_price)),
+                SimpleListItem("Placeholder", resources.getString(R.string.booking_payment)),
+                SimpleListItem("",resources.getString(R.string.booking_info))
+            ),
+            this
+        )
+
+        findViewById<FloatingActionButton>(R.id.floating_action_button).setOnClickListener {
             startActivity(Intent(this, TicketActivity::class.java))
         }
+    }
+
+    override fun onItemClicked(view: View, position: Int) {
+        // Do nothing.
     }
 }
