69d6d07165f287a03ecb96dfb60b3c8b84800314
Sandlo <128033399+Sandlo@users.noreply.github.com>
2023-11-29 16:52:31 +0100

    Event+Ticket Activity

--- app/src/main/java/com/example/eventradar/activities/EventActivity.kt
+++ app/src/main/java/com/example/eventradar/activities/EventActivity.kt
@@ -1,17 +1,46 @@
 package com.example.eventradar.activities
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.eventradar.R
+import com.example.eventradar.adapters.SimpleListAdapter
+import com.example.eventradar.data.SimpleListItem
+import com.example.eventradar.interfaces.RecyclerViewHelperInterface
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class EventActivity : BaseActivity() {
+class EventActivity : BaseActivity(), RecyclerViewHelperInterface {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event)
 
-        findViewById<Button>(R.id.dummy_button).setOnClickListener {
+        findViewById<FloatingActionButton>(R.id.buy).setOnClickListener {
             startActivity(Intent(this, BookingActivity::class.java))
         }
+
+        val recyclerView = findViewById<RecyclerView>(R.id.list)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = SimpleListAdapter(
+            listOf(
+                SimpleListItem(resources.getString(R.string.description), "PLACEHOLDER"),
+                SimpleListItem("Luca", resources.getString(R.string.organizer)),
+                SimpleListItem("20. April 2045", resources.getString(R.string.`when`)),
+                SimpleListItem("Bei Luca im Garten", resources.getString(R.string.where))
+            ),
+            this
+        )
+
+        findViewById<View>(R.id.frame).setBackgroundResource(R.drawable.elena_de_soto)
+        findViewById<TextView>(R.id.title).text = "PLACEHOLDER"
+        findViewById<TextView>(R.id.summary).text = "PLACEHOLDER"
+    }
+
+    override fun onItemClicked(view: View, position: Int) {
+        // Do nothing.
     }
 }
--- app/src/main/java/com/example/eventradar/activities/TicketActivity.kt
+++ app/src/main/java/com/example/eventradar/activities/TicketActivity.kt
@@ -1,11 +1,36 @@
 package com.example.eventradar.activities
 
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.eventradar.R
+import com.example.eventradar.adapters.SimpleListAdapter
+import com.example.eventradar.data.SimpleListItem
+import com.example.eventradar.interfaces.RecyclerViewHelperInterface
 
-class TicketActivity : BaseActivity() {
+class TicketActivity : BaseActivity(), RecyclerViewHelperInterface {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ticket)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.list)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = SimpleListAdapter(
+            listOf(
+                SimpleListItem("", resources.getString(R.string.ticket_info)),
+                SimpleListItem("PLACEHOLDER", resources.getString(R.string.what)),
+                SimpleListItem("20. April 2045", resources.getString(R.string.`when`)),
+                SimpleListItem("Bei Luca im Garten", resources.getString(R.string.where)),
+                SimpleListItem(resources.getString(R.string.ticket_cancel))
+            ),
+            this
+        )
     }
+
+    override fun onItemClicked(view: View, position: Int) {
+        // Do nothing.
+    }
+
+
 }
