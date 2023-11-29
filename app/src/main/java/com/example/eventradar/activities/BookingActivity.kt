8c01655ab2f2f760817b52b79fc68a0135f5c3a6
TimIsKing <zuehlke.tim2@gmail.com>
2023-11-29 15:57:31 +0100

    Booking Liste und Buttom

--- app/src/main/java/com/example/eventradar/activities/BookingActivity.kt
+++ app/src/main/java/com/example/eventradar/activities/BookingActivity.kt
@@ -2,16 +2,41 @@ package com.example.eventradar.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.eventradar.R
+import com.example.eventradar.adapters.SimpleListAdapter
+import com.example.eventradar.data.SimpleListItem
+import com.example.eventradar.helpers.Database
+import com.example.eventradar.interfaces.RecyclerViewHelperInterface
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class BookingActivity : BaseActivity() {
+class BookingActivity : BaseActivity(), RecyclerViewHelperInterface {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_booking)
+        val recyclerView = findViewById<RecyclerView>(R.id.list)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = SimpleListAdapter(
+            listOf(
+                SimpleListItem("Placeholder", "Titel das Events"),
+                SimpleListItem("Placeholder","Preis inkl. MwSt."),
+                SimpleListItem("Placeholder","Zahlungsmethode"),
+                SimpleListItem("Placeholder","Eine ausführliche Rechnung und das Ticket bekommst du im Anschluss per Mail zugesandt. Das Ticket ist auch jeder Zeit in der App abrufbar.")
+            ),
+            this
+        )
 
-        findViewById<Button>(R.id.dummy_button).setOnClickListener {
+
+
+        findViewById<FloatingActionButton>(R.id.floating_action_button).setOnClickListener {
             startActivity(Intent(this, TicketActivity::class.java))
         }
+    }
+
+    override fun onItemClicked(view: View, position: Int) {
+        TODO("Not yet implemented")
     }
 }
