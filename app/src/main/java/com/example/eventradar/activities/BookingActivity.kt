ce1308dafacca74cbda598b64e8d4839b9687c0d
domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2023-11-30 10:02:02 +0100

    Format code

--- app/src/main/java/com/example/eventradar/activities/BookingActivity.kt
+++ app/src/main/java/com/example/eventradar/activities/BookingActivity.kt
@@ -3,13 +3,11 @@ package com.example.eventradar.activities
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.eventradar.R
 import com.example.eventradar.adapters.SimpleListAdapter
 import com.example.eventradar.data.SimpleListItem
-import com.example.eventradar.helpers.Database
 import com.example.eventradar.interfaces.RecyclerViewHelperInterface
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
@@ -17,19 +15,18 @@ class BookingActivity : BaseActivity(), RecyclerViewHelperInterface {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_booking)
+
         val recyclerView = findViewById<RecyclerView>(R.id.list)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = SimpleListAdapter(
             listOf(
-                SimpleListItem("Placeholder", "Titel das Events"),
-                SimpleListItem("Placeholder","Preis inkl. MwSt."),
-                SimpleListItem("Placeholder","Zahlungsmethode"),
-                SimpleListItem("Placeholder","Eine ausführliche Rechnung und das Ticket bekommst du im Anschluss per Mail zugesandt. Das Ticket ist auch jeder Zeit in der App abrufbar.")
+                SimpleListItem("Placeholder", resources.getString(R.string.booking_title)),
+                SimpleListItem("Placeholder", resources.getString(R.string.booking_price)),
+                SimpleListItem("Placeholder", resources.getString(R.string.booking_payment)),
+                SimpleListItem("",resources.getString(R.string.booking_info))
             ),
             this
         )
-
-
 
         findViewById<FloatingActionButton>(R.id.floating_action_button).setOnClickListener {
             startActivity(Intent(this, TicketActivity::class.java))
@@ -37,6 +34,6 @@ class BookingActivity : BaseActivity(), RecyclerViewHelperInterface {
     }
 
     override fun onItemClicked(view: View, position: Int) {
-        TODO("Not yet implemented")
+        // Do nothing.
     }
 }
