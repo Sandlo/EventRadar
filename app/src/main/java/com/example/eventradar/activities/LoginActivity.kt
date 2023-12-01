58815c02c856180b05f92e0f1579ecc69a51750e
joooel19 <111013947+joooel19@users.noreply.github.com>
2023-12-01 11:21:56 +0100

    update activity_login

--- app/src/main/java/com/example/eventradar/activities/LoginActivity.kt
+++ app/src/main/java/com/example/eventradar/activities/LoginActivity.kt
@@ -4,24 +4,39 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import com.example.eventradar.R
+import com.example.eventradar.helpers.OutOfScopeDialog
 import com.example.eventradar.helpers.Preferences
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        findViewById<Button>(R.id.dummy_button).setOnClickListener {
+        findViewById<Button>(R.id.guest).setOnClickListener {
             Preferences.setLoggedIn(this, true)
             startActivity(Intent(this, MainActivity::class.java))
         }
 
-        findViewById<Button>(R.id.second_dummy_button).setOnClickListener {
+        findViewById<Button>(R.id.register).setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
         }
 
-        findViewById<Button>(R.id.third_dummy_button).setOnClickListener {
+        findViewById<Button>(R.id.reset_password).setOnClickListener {
+            OutOfScopeDialog.show(this)
+        }
+
+        findViewById<Button>(R.id.google_sign_in_button).setOnClickListener {
+            OutOfScopeDialog.show(this)
+        }
+
+        findViewById<Button>(R.id.facebook_sign_in_button).setOnClickListener {
+            OutOfScopeDialog.show(this)
+        }
+
+        findViewById<FloatingActionButton>(R.id.continue_button).setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
+
     }
 }
