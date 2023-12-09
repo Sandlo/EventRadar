64737dfbc7a7ccf695336a9789b13a9bf0236f13
Sandlo <128033399+Sandlo@users.noreply.github.com>
2023-12-09 22:02:43 +0100

    Data doc strings added

--- app/src/main/java/com/example/eventradar/data/SimpleListItem.kt
+++ app/src/main/java/com/example/eventradar/data/SimpleListItem.kt
@@ -1,7 +1,14 @@
 package com.example.eventradar.data
 
+/**
+ * Data class representing a simple list item with a title, summary, and an icon.
+ *
+ * @param title The title of the list item.
+ * @param summary The summary or description of the list item.
+ * @param icon The resource ID of the icon associated with the list item.
+ */
 data class SimpleListItem(
     val title: String = "",
     val summary: String = "",
-    val icon: Int = android.R.color.transparent,
+    val icon: Int = android.R.color.transparent
 )
