c630542781efb1ba01e8fcf5068a90820ad2150d
Sandlo <128033399+Sandlo@users.noreply.github.com>
2023-12-09 21:52:50 +0100

    Doc string interfaces added

--- app/src/main/java/com/example/eventradar/interfaces/RecyclerViewHelperInterface.kt
+++ app/src/main/java/com/example/eventradar/interfaces/RecyclerViewHelperInterface.kt
@@ -1,5 +1,13 @@
 package com.example.eventradar.interfaces
 
+/**
+ * Interface zur Bereitstellung einer Callback-Funktion für Klickereignisse in RecyclerView-Elementen.
+ */
 interface RecyclerViewHelperInterface {
+    /**
+     * Wird aufgerufen, wenn ein Element in einem RecyclerView angeklickt wird.
+     *
+     * @param position Die Position des angeklickten Elements im RecyclerView.
+     */
     fun onItemClicked(position: Int)
 }
