48f708c55ba73792ad42429896d26b807b80d522
Sandlo <128033399+Sandlo@users.noreply.github.com>
2023-12-09 21:51:16 +0100

    dao doc strings added

--- app/src/main/java/com/example/eventradar/data/dao/AddressDao.kt
+++ app/src/main/java/com/example/eventradar/data/dao/AddressDao.kt
@@ -4,8 +4,14 @@ import androidx.room.Dao
 import androidx.room.Insert
 import com.example.eventradar.data.entities.Address
 
+/**
+ * Data Access Object (DAO) Schnittstelle für den Zugriff auf Adressdaten in der Datenbank.
+ */
 @Dao
 interface AddressDao {
+    /**
+     * Fügt einen oder mehrere Adressdatensätze in die Datenbank ein.
+     */
     @Insert
     suspend fun insertAll(vararg addresses: Address)
 }
--- app/src/main/java/com/example/eventradar/data/dao/EventInterestDao.kt
+++ app/src/main/java/com/example/eventradar/data/dao/EventInterestDao.kt
@@ -4,8 +4,14 @@ import androidx.room.Dao
 import androidx.room.Insert
 import com.example.eventradar.data.entities.EventInterest
 
+/**
+ * Data Access Object (DAO) Schnittstelle für den Zugriff auf Event-Interessendaten in der Datenbank.
+ */
 @Dao
 interface EventInterestDao {
+    /**
+     * Fügt einen oder mehrere Event-Interessensdatensätze in die Datenbank ein.
+     */
     @Insert
     suspend fun insertAll(vararg interests: EventInterest)
 }
